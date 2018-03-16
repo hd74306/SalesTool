7a7212efa722c58e7116136214f7a5e6ec33d092
hareeshdevarasetty <hareeshdevarasetty@hareeshs-air.fios-router.home>
2018-03-15 21:13:14 -0400

    Initial Commit

---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package ca.demo.salestool;
+
+public class SalesData {
+    int data[] = {0, 42, 4};
+    
+    public void display(){
+    	System.out.println("TODO:Implement data display");
+    }
+}
